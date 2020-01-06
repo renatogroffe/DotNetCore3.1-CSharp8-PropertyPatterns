1b4bdc19a873b7d0c30303e9e9d5635e8068de97
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-05 22:09:37 -0300

    .NET Core 3.1 + Console Application + C #8.0 + Property Patterns
    .NET Core 3.1 + Console Application + C #8.0 + Property Patterns

---
+++ ExemploPropertyPatterns/Passagem.cs
@@ -0,0 +1,8 @@
+namespace ExemploPropertyPatterns
+{
+    public class Passagem
+    {
+        public string SiglaRegiao { get; set; }
+        public double PrecoBase { get; set; }
+    }
+}
---
+++ ExemploPropertyPatterns/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExemploPropertyPatterns
+{
+    class Program
+    {
+        private static double AplicarAdicional(Passagem passagem) =>
+            passagem switch
+            {
+                { SiglaRegiao: "N" } => passagem.PrecoBase * 1.5,
+                { SiglaRegiao: "NE" } => passagem.PrecoBase * 1.4,
+                { SiglaRegiao: "CO" } => passagem.PrecoBase * 1.3,
+                { SiglaRegiao: "S" } => passagem.PrecoBase * 1.1,
+                _ => passagem.PrecoBase
+            };
+
+        static void Main()
+        {
+            Passagem p1 = new Passagem() { SiglaRegiao = "NE", PrecoBase = 200.0 };
+            Console.WriteLine($"Passagem 1 - Região: {p1.SiglaRegiao}, " +
+                $"Preço Base: {p1.PrecoBase}, Preço Final: " + AplicarAdicional(p1));
+
+            Passagem p2 = new Passagem() { SiglaRegiao = "SE", PrecoBase = 50.0 };
+            Console.WriteLine($"Passagem 2 - Região: {p2.SiglaRegiao}, " +
+                $"Preço Base: {p2.PrecoBase}, Preço Final: " + AplicarAdicional(p2));
+        }
+    }
+}
